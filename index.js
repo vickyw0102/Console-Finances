8d1dbb33743f4c5e11901e38db5dda046bffd66a
vicky wong <vicky.boci@gmail.com>
2023-12-16 20:45:01 +0800

    completed the caluation of total and difference

--- index.js
+++ index.js
@@ -88,16 +88,19 @@ var finances = [
 ];
 var totalMonths= finances.length;
 var total =0;
-var change;
+var change=0;
 var date;  //e.g. Jan-2010; =currenetMonth[0]
 var amount; //e.g 867884; =crrentMonth[1]
 var currentMonth; //e.g. Jan-2010 & 867884 together = finances i
 var previousAmount;
-var greatestIncrease;
-var greatestDecrease;
+var greatestIncrease=0;
+var greatestIncreaseMonth="";
+var greatestDecrease=0;
+var greatestDecreaseMonth="";
 
-
-console.log(`The total number of the months is ${finances.length}`);
+console.log ('Financal Analysis');
+console.log(`The total number of the months is ${totalMonths}`);
+console.log('------------');
 
 
     for (var i=0; i< totalMonths; i++){
@@ -105,14 +108,26 @@ console.log(`The total number of the months is ${finances.length}`);
         date = currentMonth[0];
         amount= currentMonth[1]; 
         total += amount;
-        if (i >0) change = amount -previousAmount;
-        
-        
-        
-        console.log(change);
-    
-        console.log(total);
-    
-        // var change =amount - currentMonth[1+1];
-    // total + = change;
+        if (i >0) {
+            change += amount - previousAmount;
+          if(amount - previousAmount>greatestIncrease){
+            greatestIncreaseMonth =date;
+            greatestIncrease = amount -previousAmount;
+          }
+
+          if(amount-previousAmount <greatestDecrease){
+            greatestDecreaseMonth=date;
+            greatestDecrease=amount - previousAmount;
+          } 
+
+        } 
+        previousAmount=amount;
     }
+        
+console.log(`Total: \$${total}`);
+var averageChange = Math.round((change/totalMonths)*100)/100;
+console.log(`Average Change: ${averageChange}`);
+console.log(`Greatest Increase in Profile/Losses: ${greatestIncreaseMonth}`);
+console.log( `(\$${greatestIncrease})`);
+console.log(`Greatest Decrease in Profile/Loss: ${greatestDecreaseMonth}`);
+console.log(`(\$${greatestDecrease})`);
